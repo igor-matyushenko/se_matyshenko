f7116f165c0c0ca93c33dfb0f794ea234eb4a786
Igor Matyushenko <ig0r.matyushenk0@mail.ru>
2019-08-13 10:14:16 +0300

    Modul_9

--- src/sef/module9/activity/RadarContactImpl.java
+++ src/sef/module9/activity/RadarContactImpl.java
@@ -16,8 +16,13 @@ public class RadarContactImpl implements RadarContact {
 	 * @param bearing the contact's bearing
 	 * @param distance the contact's distance
 	 */
+	String contactID;
+	double bearing;
+	double distance;
 	public RadarContactImpl(String contactID, double bearing, double distance){
-		
+		setContactID(contactID);
+		setBearing(bearing);
+		setDistance(distance);
 		
 	}
 	
@@ -26,7 +31,7 @@ public class RadarContactImpl implements RadarContact {
 	 * @see sef.module8.activity.RadarContact#getBearing()
 	 */
 	public final double getBearing() {
-		return 1;
+		return bearing;
 	}
 	
 
@@ -34,42 +39,42 @@ public class RadarContactImpl implements RadarContact {
 	 * @see sef.module8.activity.RadarContact#setBearing(double)
 	 */
 	public final void setBearing(double bearing) {
-		
-		
+		bearing %=360;
+		this.bearing  = (bearing<0)? bearing+360 :(bearing == -0.0 || bearing == -360)? 0.0: bearing;
 	}
 
 	/* (non-Javadoc)
 	 * @see sef.module8.activity.RadarContact#getDistance()
 	 */
 	public final double getDistance() {
-		return 0;
+		return distance;
 	}
 
 	/* (non-Javadoc)
 	 * @see sef.module8.activity.RadarContact#setDistance(double)
 	 */
 	public final void setDistance(double distance) {
-		
+		this.distance = (distance<0)? 0 : distance;
 	}
 
 	/* (non-Javadoc)
 	 * @see sef.module8.activity.RadarContact#getContactID()
 	 */
 	public final String getContactID() {
-		return "";
+		return contactID;
 	}
 
 	/* (non-Javadoc)
 	 * @see sef.module8.activity.RadarContact#setContactID(java.lang.String)
 	 */
 	public final void setContactID(String contactID) {
-		;
+		this.contactID = contactID;
 	}
 
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
 	public String toString(){
-		return "";
+		return "contact: "+getContactID()+"\nbearing: "+ getBearing()+ "\ndistance: " + getDistance();
 	}
 }
